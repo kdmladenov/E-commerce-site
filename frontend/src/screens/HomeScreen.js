9c4facfbabe6980b480dbf094808b48d0eed6cba
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-08-20 16:13:20 +0300

    HomeScreen finished

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,52 +1,57 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productAction';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import ProductCard from '../components/ProductCard';
 import './styles/HomeScreen.css';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
-  const productlist = useSelector(state => state.productList)
-  const {loading, products, error} = productlist
+  const productlist = useSelector((state) => state.productList);
+  const { loading, products, error } = productlist;
 
   console.log(products);
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  const productsToShow = products?.map(product => (
+  const productsToShow = products?.map((product) => (
     <ProductCard
-            id={product.id}
-            title={product.title}
-            image={product.image}
-            price={product.price}
-            rating={product.rating}
-          />
+      id={product.id}
+      title={product.title}
+      image={product.image}
+      price={product.price}
+      rating={product.rating}
+    />
   ));
 
   return (
     <div className="home">
-      <div className="home_container">
-        <img
-          src="https://static.vecteezy.com/system/resources/previews/001/183/315/original/summer-sale-online-shopping-banner-vector.jpg"
-          alt="banner"
-        />
-        {products && (<>
-        <div className="home_row">
-          {productsToShow[0]}
-          {productsToShow[1]}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div className="home_container">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/001/183/315/original/summer-sale-online-shopping-banner-vector.jpg"
+            alt="banner"
+          />
+
+          <div className="home_row">
+            {productsToShow[0]}
+            {productsToShow[1]}
+          </div>
+          <div className="home_row">
+            {productsToShow[2]}
+            {productsToShow[3]}
+            {productsToShow[5]}
+          </div>
+          <div className="home_row">{productsToShow[4]}</div>
         </div>
-        <div className="home_row">
-          {productsToShow[2]}
-          {productsToShow[3]}
-          {productsToShow[4]}
-        </div>
-        <div className="home_row">
-          {productsToShow[5]}
-        </div>
-        </>)}
-      </div>
+      )}
     </div>
   );
 };
