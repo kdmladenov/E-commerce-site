053fb106783b6cbc844f1d2006a37d95f74f5a7c
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-15 13:24:58 +0300

    Categories Mega Menu code refactored

---
+++ frontend/src/constants/for-developing/categoriesMega.js
@@ -0,0 +1,181 @@
+export const categories = [
+  {
+    'Phones, Tablets & Laptops': [
+      {
+        'Mobile Phones & Accessories': [
+          'Mobile phones',
+          'Mobile phone cases',
+          'Protective films for mobile phones',
+          'Mobile phone chargers',
+          'Mobile phone batteries',
+          'Bluetooth headset',
+          'Data cables for mobile phones',
+          'Mobile phone stands and docking stations',
+          'External batteries for mobile phones',
+          'Memory cards'
+        ]
+      },
+      {
+        'Tablets, Laptops & Accessories': [
+          'Tablets',
+          'Tablet cases',
+          'Protective foils for tablets',
+          'Tablet chargers',
+          'Car racks & docking',
+          'Tablet keyboards',
+          'Tablet cables and adapters',
+          'Other tablet accessories',
+          'Laptops',
+          'Laptop bags',
+          'Laptop memory',
+          'Hard drives for laptops',
+          'Cooling pads',
+          'Laptop chargers',
+          'Laptop batteries',
+          'Other accessories',
+          'Unpacked laptops'
+        ]
+      },
+      {
+        'Gadgets & Smart Technologies': [
+          'Smartwatch',
+          'Fitness bracelets',
+          'Other widgets',
+          'VR glasses',
+          'Smart home solutions and sensors'
+        ]
+      }
+    ]
+  },
+  {
+    'Computers & Peripherals': [
+      {
+        'Desktops & Monitors': ['Desktops', 'LCD & LED Monitors', 'Ax. for monitors']
+      },
+      {
+        'PC Components': [
+          'Processors',
+          'Video cards',
+          'motherboards',
+          'RAM',
+          'Hard Drives',
+          'Computer cases',
+          'Power supplies',
+          'Solid-State Drive (SSD)',
+          'IT accessories',
+          'Sound cards'
+        ]
+      },
+      {
+        Software: ['Office & Desktop applications', 'Operating Systems', 'Antivirus programs']
+      },
+      {
+        Peripherals: [
+          'USB memory',
+          'External Hard Drives',
+          'External SSDs',
+          'Keyboards',
+          'Mice',
+          'Computer headphones',
+          'External optical devices',
+          'Webcams',
+          'Graphic tablets'
+        ]
+      },
+      {
+        'Printers and Scanners': [
+          'Laser printers',
+          'Inkjet printers',
+          'Photo printers',
+          'Cartridges, toners and other consumables'
+        ]
+      },
+      {
+        'Wireless & Surveillance Systems': [
+          'Wireless routers',
+          'Network antennas and accessories',
+          'Wireless adapters',
+          'Surveillance cameras',
+          'Network-attached storage'
+        ]
+      }
+    ]
+  }
+  // {
+  //   'TV, Audio & Photo': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   Gaming: [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Large appliances': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Small appliances': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   Fashion: [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Health and Beauty': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Home, Garden & Petshop': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   "Toys & Children's Items": [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Sports & leisure': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Auto & DIY': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // },
+  // {
+  //   'Books, Office & Food': [
+  //     {
+  //       '': []
+  //     }
+  //   ]
+  // }
+];
---
+++ frontend/src/constants/for-developing/mainCategoryIcons.js
@@ -0,0 +1,15 @@
+export const categoryIcons = {
+  'Phones, Tablets & Laptops': 'fas fa-mobile-alt',
+  'Computers & Peripherals': 'fas fa-laptop',
+  'TV, Audio & Photo': 'fas fa-tv',
+  Gaming: 'fas fa-gamepad',
+  'Large appliances': 'fas fa-expand',
+  'Small appliances': 'fas fa-blender',
+  Fashion: 'fas fa-tshirt',
+  'Health and Beauty': 'fas fa-stethoscope',
+  'Home, Garden & Petshop': 'fas fa-home',
+  "Toys & Children's Items": 'fas fa-child',
+  'Sports & leisure': 'fas fa-biking',
+  'Auto & DIY': 'fas fa-car',
+  'Books, Office & Food': 'fas fa-book'
+};
