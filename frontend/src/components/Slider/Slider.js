38ef2402568315402e5c1850e002ae45510491a2
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-18 17:23:13 +0300

    Slider updated with CSS in props

--- frontend/src/components/Slider/Slider.js
+++ frontend/src/components/Slider/Slider.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import SliderBtn from './SliderBtn';
 import './styles/Slider.css';
 
-const Slider = ({ images }) => {
+const Slider = ({ images, dots, prevBtnClass, nextBtnClass }) => {
   const [slideIndex, setSlideIndex] = useState(0);
 
   const prevSlide = () => {
@@ -16,20 +16,23 @@ const Slider = ({ images }) => {
   return (
     <main className="slider_container">
       {images.map((image, index) => (
-        <div className={slideIndex === index ? 'slide active' : 'slide'}>
+        <div className={slideIndex === index ? 'slide active' : 'slide'} key={index}>
           <img src={image} alt={image} />
         </div>
       ))}
-      <SliderBtn moveSlide={prevSlide} direction={'prev'} />
-      <SliderBtn moveSlide={nextSlide} direction={'next'} />
-      <div className="slider_dots">
-        {images.map((_, index) => (
-          <div
-            className={slideIndex === index ? 'dot active' : 'dot'}
-            onClick={() => setSlideIndex(index)}
-          />
-        ))}
-      </div>
+      <SliderBtn moveSlide={prevSlide} direction={'prev'} style={prevBtnClass} />
+      <SliderBtn moveSlide={nextSlide} direction={'next'} style={nextBtnClass} />
+      {dots && (
+        <div className="slider_dots">
+          {images.map((_, index) => (
+            <div
+              className={slideIndex === index ? 'dot active' : 'dot'}
+              onClick={() => setSlideIndex(index)}
+              key={index}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 };
--- frontend/src/components/Slider/SliderBtn.js
+++ frontend/src/components/Slider/SliderBtn.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles/Slider.css';
 
-const SliderBtn = ({ moveSlide, direction }) => {
+const SliderBtn = ({ moveSlide, direction, style }) => {
   return (
     <button
-      className={direction === 'next' ? 'slider_btn next' : 'slider_btn prev'}
+      className={direction === 'next' ? `slider_btn next ${style}` : `slider_btn prev ${style}`}
       onClick={moveSlide}
     >
       <i className={direction === 'next' ? 'fas fa-chevron-right' : 'fas fa-chevron-left'}></i>
