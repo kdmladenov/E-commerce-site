5c5cb50dc4bf0c3c5cea78a5c24deaf8d706885a
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-21 12:07:46 +0300

    Refactored rating code

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,49 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const STAR_COUNT = 5;
+
 const Rating = ({ rating, text, color }) => {
   return (
     <div className="rating">
-      <span>
-        <i
-          style={{ color }}
-          className={
-            rating >= 1 ? 'fas fa-star' : rating >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'
-          }
-        />
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            rating >= 2 ? 'fas fa-star' : rating >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'
-          }
-        />
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            rating >= 3 ? 'fas fa-star' : rating >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'
-          }
-        />
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            rating >= 4 ? 'fas fa-star' : rating >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'
-          }
-        />
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            rating >= 5 ? 'fas fa-star' : rating >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'
-          }
-        />
-      </span>
+      {Array.from({ length: STAR_COUNT }).map((_, index) => (
+        <span key={index}>
+          <i
+            style={{ color }}
+            className={
+              rating >= index + 1
+                ? 'fas fa-star'
+                : rating >= index + 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          />
+        </span>
+      ))}
       <span>{text && text}</span>
     </div>
   );
