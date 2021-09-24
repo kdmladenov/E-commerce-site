dce46a0c74aab6601aecdc45956b06db5edaece9
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-24 21:31:07 +0300

    Flat design on Header,Footer and Megamenu

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -4,7 +4,7 @@ import './styles/CheckoutSteps.css';
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
-    <div className="flex m-auto">
+    <div className="checkout_steps flex m-auto">
       {step1 ? <Link to="/login">Sign In</Link> : <Link disabled>Sign In</Link>}
       {step2 ? <Link to="/shipping">Shipping</Link> : <Link disabled>Shipping</Link>}
       {step3 ? <Link to="/payment">Payment</Link> : <Link disabled>Payment</Link>}
