7a7cfdce2a2b74fe66cec00243b8c800122a2067
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-25 10:00:32 +0300

    Frontend Delete Product implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { productDeleteReducer, productDetailsReducer, productListReducer } from './reducers/productReducers';
 import {
   userDeleteReducer,
   userDetailsReducer,
@@ -18,6 +18,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
