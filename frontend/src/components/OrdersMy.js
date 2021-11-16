ec5fe2d34bde4208b8911a716da16f1e7dccabee
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-11-16 11:13:13 -0800

    My Orders component finished

---
+++ frontend/src/components/OrdersMy.js
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+import Accordion from './Accordion';
+import { useDispatch, useSelector } from 'react-redux';
+import { listMyOrders } from '../actions/orderActions';
+import Loader from './Loader';
+import Message from './Message';
+import { getDate, numberDecimalFix } from '../constants/utility-functions/utility-functions';
+import OrderDetails from './OrderDetails';
+import './styles/OrdersMy.css';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
+import Button from './Button';
+
+const OrdersMy = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const orderMyList = useSelector((state) => state.orderMyList);
+  const { loading, error, orders } = orderMyList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const addToCartHandler = (id) => {
+    history.push(`/cart/${id}?qty=1`);
+  };
+
+  useEffect(() => {
+    dispatch(listMyOrders());
+  }, [dispatch, userInfo]);
+
+  return (
+    <div className="orders">
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message type="error">{error}</Message>
+      ) : orders?.length > 0 ? (
+        <Accordion>
+          {orders?.map((order) => (
+            <Accordion.Item id={order.orderId}>
+              <Accordion.Header>
+                <Accordion.Title>
+                  <div className="order_title_left">
+                    <div>
+                      <span>ORDER DATE</span>
+                      <span>{getDate(order.orderDate)}</span>
+                    </div>
+                    <div>
+                      <span>TOTAL</span>
+                      <span>$ {order.totalPrice}</span>
+                    </div>
+                    <div>
+                      <span>SHIP TO</span>
+                      <span>{`${order.shippingAddress} ${
+                        order.shippingAddress2 !== null ? order.shippingAddress2 : ''
+                      }, ${order.shippingCity}, ${order.shippingState}, ${
+                        order.shippingCountry
+                      }`}</span>
+                    </div>
+                  </div>
+                </Accordion.Title>
+                <Accordion.ButtonGroup>
+                  <div className="order_title_right">
+                    <span>{`ORDER # ${order.orderId}`}</span>
+                    <Link to={`/order/${order.orderId}`}>
+                      <Button classes="text">View order details</Button>
+                    </Link>
+                  </div>
+                </Accordion.ButtonGroup>
+              </Accordion.Header>
+              <Accordion.Body>
+                {order?.orderItems?.length === 0 ? (
+                  <Message type="error">Order is empty</Message>
+                ) : (
+                  <ul>
+                    {order?.orderItems?.map((item) => (
+                      <li key={item.id}>
+                        <div className="order_item">
+                          <div className="image">
+                            <img src={item.image} alt={item.title} />
+                          </div>
+                          <div className="title">
+                            <Link to={`/products/${item.id}`}>{item.title}</Link>
+                          </div>
+                          <div className="total">
+                            {`$ ${numberDecimalFix(item.price)} x ${item.qty}`}
+                          </div>
+                          <Button
+                            onClick={() => addToCartHandler(item.id)}
+                            classes="small"
+                            className="order_item_btn"
+                          >
+                            Add to cart
+                          </Button>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      ) : (
+        <Message type="success">Ask Question Box</Message>
+      )}
+    </div>
+  );
+};
+
+export default OrdersMy;
