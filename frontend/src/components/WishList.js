3510c224d049d73d4d8e35cf97a2ac55f01abdfa
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-12-09 17:13:47 +0200

    WishList - search, sort, paginate

--- frontend/src/components/WishList.js
+++ frontend/src/components/WishList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import { deleteWishFromList, listWishedItems } from '../actions/wishListActions';
@@ -12,10 +12,21 @@ import { numberDecimalFix } from '../constants/utility-functions';
 import Popover from './Popover';
 import RatingWidget from './RatingWidget';
 import WishListCard from './WishListCard';
+import SearchBox from './SearchBox';
+import DropdownSelect from './DropdownSelect';
+import { productListPageSizeOptionsMap, productListSortOptionsMap } from '../constants/inputMaps';
+import Pagination from './Pagination';
 
 const WishList = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const [endpoint, setEndpoint] = useState({
+    page: 'page=1&',
+    pageSize: 'pageSize=12&',
+    sort: 'sort=price asc&',
+    search: ''
+  });
 
   const wishListItems = useSelector((state) => state.wishListItems);
   const { loading: loadingWishList, wishList, error: errorWishList } = wishListItems;
@@ -26,17 +37,55 @@ const WishList = () => {
   const wishListCardsToShow = wishList?.map((wish) => <WishListCard wish={wish} />);
 
   useEffect(() => {
-    dispatch(listWishedItems());
-  }, [dispatch, successDeleteWish]);
+    const { page, pageSize, sort, search } = endpoint;
 
-  return loadingWishList ? (
-    <Loader />
-  ) : errorWishList ? (
-    <Message type="error">{errorWishList}</Message>
-  ) : wishList.length === 0 ? (
-    <h2>Your Wish List Is Empty</h2>
-  ) : (
-    <ul className="wish_list_items">{wishListCardsToShow}</ul>
+    dispatch(listWishedItems(`${page}${pageSize}${sort}${search}`));
+  }, [dispatch, successDeleteWish, endpoint]);
+
+  return (
+    <div className="wish_list">
+      <div className="header">
+        <SearchBox
+          updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+          resource="orders"
+        />
+        <div className="dropdown_group_container">
+          <DropdownSelect
+            name="pageSize"
+            updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+            query={endpoint}
+            labelStart="Page size"
+            optionsMap={productListPageSizeOptionsMap}
+          />
+          <DropdownSelect
+            name="sort"
+            updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+            query={endpoint}
+            labelStart="Sort by"
+            optionsMap={productListSortOptionsMap}
+          />
+        </div>
+      </div>
+      {loadingWishList ? (
+        <Loader />
+      ) : errorWishList ? (
+        <Message type="error">{errorWishList}</Message>
+      ) : wishList.length === 0 ? (
+        <h2>Your Wish List Is Empty</h2>
+      ) : (
+        <ul className="wish_list_items">{wishListCardsToShow}</ul>
+      )}
+      <div className="footer">
+        {wishList?.length > 0 && (
+          <Pagination
+            updateQuery={(prop, value) => setEndpoint({ ...endpoint, [prop]: value })}
+            currentPage={+endpoint.page.slice('page='.length).replace('&', '')}
+            pageSize={+endpoint.pageSize.slice('pageSize='.length).replace('&', '')}
+            totalItems={wishList[0].totalDBItems}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
