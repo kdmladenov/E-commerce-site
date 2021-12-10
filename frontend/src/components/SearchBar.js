6eb035912b2c9d4abbf5c0ac828ecd79aedabd9c
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-12-10 16:03:46 +0200

    Header Search Bar updated

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/SearchBar.css';
 import { suggestions } from '../constants/for-developing/suggestions';
 import { trending } from '../constants/for-developing/trending';
@@ -7,89 +7,38 @@ import {
   isKeywordInString,
   keywordInString
 } from '../constants/utility-functions';
-import { categories } from '../constants/for-developing/categoriesMega';
-import { categoryIcons } from '../constants/for-developing/mainCategoryIcons';
 import { useHistory } from 'react-router';
 import Tooltip from './Tooltip';
+import useOutsideClick from '../hooks/useOutsideClick';
 
-const AUTOCOMPLETE_SUGGESTIONS_COUNT = 5;
-const TRENDING_SEARCHES_COUNT = 5;
-const PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH = 10;
+const PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH = 5;
 
 const SearchBar = () => {
+  const history = useHistory();
+
   const [searchTerm, setSearchTerm] = useState('');
   const [showDropdown, setShowDropdown] = useState(false);
-  const [showTrendingSearches, setShowTrendingSearches] = useState(false);
-  // const [productCategory, setProductCategory] = useState('');
 
-  const previousSearchesFromLocalStorage = localStorage.getItem('previousSearches')
-    ? JSON.parse(localStorage.getItem('previousSearches'))
-    : [];
-  const [previousSearches, setPreviousSearches] = useState(previousSearchesFromLocalStorage);
+  const [previousSearches, setPreviousSearches] = useState([]);
+  console.log(Array.isArray(previousSearches), 'array');
+  console.log(previousSearches.length, 'length');
 
-  const previousSearchesToRender = previousSearches
-    ?.map((previousSearch, index) => (
-      <li key={`${previousSearch}_${index}`}>
-        <div>
-          <i className="fa fa-search"></i>
-          {previousSearch}
-        </div>
-      </li>
-    ))
-    .slice(0, AUTOCOMPLETE_SUGGESTIONS_COUNT);
-
-  const trendingSearchesToRender = trending
-    .map((suggestion, index) => (
-      <li key={`${suggestion}_${index}`} onClick={() => setSearchTerm(suggestion)}>
-        <div>
-          <i className="fa fa-search"></i>
-          {suggestion}
-        </div>
-      </li>
-    ))
-    .slice(0, TRENDING_SEARCHES_COUNT);
-
-  // const categoriesDropdownToRender = alphabeticalSort(
-  //   productCategory.length > 0
-  //     ? [...Object.keys(categories).filter((category) => productCategory !== category), 'All']
-  //     : Object.keys(categories)
-  // ).map((category) => (
-  //   <li key={category} onClick={() => categorySelectionHandler(category)}>
-  //     <div>
-  //       <i className={`${categoryIcons[category]} main`}></i>
-  //       {category}
-  //     </div>
-  //   </li>
-  // ));
-
-  const history = useHistory();
-  // const endpoint = history.location.search.slice(1).split('&');
-  // const page = endpoint.find((i) => i.startsWith('page='))
-  //   ? `${endpoint.find((i) => i.startsWith('page='))}&`
-  //   : '';
-  // const pageSize = endpoint.find((i) => i.startsWith('pageSize='))
-  //   ? `${endpoint.find((i) => i.startsWith('pageSize='))}&`
-  //   : '';
-  // const sort = endpoint.find((i) => i.startsWith('sort='))
-  //   ? `${endpoint.find((i) => i.startsWith('sort='))}&`
-  //   : '';
-  // const order = endpoint.find((i) => i.startsWith('order='))
-  //   ? `${endpoint.find((i) => i.startsWith('order='))}&`
-  //   : '';
-
-  const dropdownButtonHandler = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  // const categorySelectionHandler = (category) => {
-  //   setProductCategory(category === 'All' ? '' : category);
-  //   setShowDropdown(false);
-  // };
+  const previousSearchesToRender =
+    previousSearches.length &&
+    previousSearches
+      ?.map((previousSearch, index) => (
+        <li key={`${previousSearch}_${index}`}>
+          <div>
+            <i className="fa fa-search"></i>
+            {previousSearch}
+          </div>
+        </li>
+      ))
+      .slice(0, PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH);
 
   const resetInputButtonHandler = () => {
     setSearchTerm('');
     setShowDropdown(false);
-    // setProductCategory('');
   };
 
   const keywordInputHandler = (e) => {
@@ -100,52 +49,69 @@ const SearchBar = () => {
 
   const keyPressHandler = (e) => {
     e.preventDefault();
+
     if (e.key === 'Enter') {
       if (searchTerm.trim()) {
+        localStorage.setItem(
+          'previousSearches',
+          JSON.stringify(
+            previousSearches?.length >= PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH
+              ? [
+                  ...previousSearches.slice(0, PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH - 1),
+                  searchTerm.trim()
+                ]
+              : [...previousSearches, searchTerm.trim()]
+          )
+        );
         history.push(`/search/${searchTerm}`);
       } else {
         history.push(`/productlist`);
       }
     }
-    // setPreviousSearches(
-    //   previousSearches?.length >= PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH
-    //     ? previousSearches?.shift()?.push(searchTerm.trim())
-    //     : previousSearches?.push(searchTerm.trim())
-    // );
-    // localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
   };
 
   const searchButtonHandler = (e) => {
     e.preventDefault();
     if (searchTerm.trim()) {
+      localStorage.setItem(
+        'previousSearches',
+        JSON.stringify(
+          previousSearches?.length >= PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH
+            ? [
+                ...previousSearches.slice(0, PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH - 1),
+                searchTerm.trim()
+              ]
+            : [...previousSearches, searchTerm.trim()]
+        )
+      );
       history.push(`/search/${searchTerm}`);
     } else {
       history.push(`/productlist`);
     }
-    // setPreviousSearches(
-    //   previousSearches?.length >= PREVIOUS_SEARCHES_ARRAY_MAX_LENGTH
-    //     ? previousSearches?.shift()?.push(searchTerm.trim())
-    //     : previousSearches?.push(searchTerm.trim())
-    // );
-    // localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
   };
 
   const inputClickHandler = () => {
-    if (!searchTerm) {
-      setShowTrendingSearches(!showTrendingSearches);
-      setShowDropdown(false);
-    }
+    setShowDropdown(true);
+    // if (!searchTerm) {
+    //   setShowDropdown(false);
+    // }
   };
 
+  let nodeRef = useOutsideClick(() => setShowDropdown(false));
+  console.log(showDropdown, 'showDropdown');
+
+  useEffect(() => {
+    setPreviousSearches(
+      localStorage.getItem('previousSearches')
+        ? JSON.parse(localStorage.getItem('previousSearches'))
+        : []
+    );
+  }, []);
+
   return (
-    <main className="search_container">
-      <div
-        className={`search_bar ${(searchTerm || showDropdown || showTrendingSearches) && 'active'}`}
-      >
+    <main className="search_container" ref={nodeRef}>
+      <div className={`search_bar ${(searchTerm || showDropdown) && 'active'}`}>
         <div className="search_inputs">
-          {/* <button type="button" onClick={dropdownButtonHandler}>
-            {productCategory ? `${productCategory}` : 'All'}
-          </button> */}
           <input
             className="search_term_input"
             type="text"
@@ -156,37 +122,7 @@ const SearchBar = () => {
             // placeholder={productCategory ? `Search in ${productCategory}` : 'Search ...'}
           />
         </div>
-        <ul>
-          {
-            searchTerm && !showDropdown ? (
-              <>
-                <h2>
-                  {previousSearchesToRender.every((item) => item === false) && 'No'}
-                  {/* Suggested
-                Searches {productCategory ? `in ${productCategory}` : 'in All Categories'} */}
-                </h2>
-                {previousSearchesToRender}
-              </>
-            ) : (
-              !searchTerm &&
-              !showDropdown &&
-              showTrendingSearches && (
-                <>
-                  <h2>Trending Searches</h2>
-                  {trendingSearchesToRender}
-                </>
-              )
-            )
-            // : (
-            //   showDropdown && (
-            //     <>
-            //       <h2>Product Categories</h2>
-            //       {categoriesDropdownToRender}
-            //     </>
-            //   )
-            // )
-          }
-        </ul>
+        {showDropdown && <ul>{previousSearchesToRender}</ul>}
         <div className="search_button_group">
           {searchTerm && (
             <button
