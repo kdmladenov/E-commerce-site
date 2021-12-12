55320aef91b74570917ea44b5a50344d2cecc5d6
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-12-12 06:13:39 +0200

    Rating widget - sends backend request, CSS

--- frontend/src/components/RatingWidget.js
+++ frontend/src/components/RatingWidget.js
@@ -1,80 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { listReviews } from '../actions/reviewActions';
-import {
-  areReviewsFiltered,
-  numberDecimalFix
-} from '../constants/utility-functions';
+import React from 'react';
+import { numberDecimalFix } from '../constants/utility-functions';
 import Rating from './Rating';
 import './styles/RatingWidget.css';
 
-const RatingWidget = ({ reviews, ratingMap, productRating, reviewCount, productId }) => {
-  const dispatch = useDispatch();
-  const [reviewsAreFiltered] = useState(areReviewsFiltered(reviews));
+const RatingWidget = ({ product, updateQuery, ratingQuery }) => {
+  const { rating, reviewCount, starOne, starTwo, starThree, starFour, starFive } = product;
+  const ratingMap = {
+    1: starOne || 0,
+    2: starTwo || 0,
+    3: starThree || 0,
+    4: starFour || 0,
+    5: starFive || 0
+  };
 
-  const rating = reviews?.reduce((acc, review) => acc + review.rating, 0) / reviews?.length;
+  const selectedRating = () => {
+    const minRating = +ratingQuery?.split('=')[1][0];
+    const maxRating = +ratingQuery?.split('=')[2][0];
+    return minRating === maxRating ? minRating : null;
+  };
 
   const filterReviews = (starCount) => {
-    if (reviews.filter((review) => review.rating === starCount).length > 0) {
-      reviewsAreFiltered
-        ? dispatch(listReviews(+productId))
-        : dispatch(listReviews(+productId, `ratingMin=${+starCount}&ratingMax=${+starCount}`));
+    const noSelection = 'ratingMin=1&ratingMax=5&';
+    if (ratingMap[starCount] > 0 && updateQuery) {
+      // Selected different rating or No selection at all
+      (ratingQuery !== noSelection && selectedRating() !== starCount) || ratingQuery === noSelection
+        ? updateQuery('rating', `ratingMin=${+starCount}&ratingMax=${+starCount}&`)
+        : updateQuery('rating', noSelection);
     }
   };
 
-
-  return reviews ? (
+  return (
     <div className="rating_widget">
       <div className="header">
         <div className="rating_stars">
           <Rating rating={rating} /> <h3>{`${numberDecimalFix(rating, 1)} out of 5`}</h3>
         </div>
-        {`${reviews.length} ratings`}
-      </div>
-      <div className="content">
-        {[5, 4, 3, 2, 1].map((starCount) => (
-          <div
-            className={`row ${
-              reviews.filter((review) => review.rating === starCount).length > 0 && 'is_rated'
-            }`}
-            onClick={() => filterReviews(starCount)}
-            key={starCount}
-          >
-            <div className="label">{`${starCount} star`}</div>
-            <div className="bar_container">
-              {reviews.filter((review) => review.rating === starCount).length > 0 && (
-                <div
-                  className="bar_level"
-                  style={{
-                    width: `${
-                      (reviews.filter((review) => review.rating === starCount).length * 100) /
-                      reviews.length
-                    }%`
-                  }}
-                ></div>
-              )}
-            </div>
-            <div className="label right">{`${numberDecimalFix(
-              (reviews.filter((review) => review.rating === starCount).length * 100) /
-                reviews.length,
-              0
-            )}%`}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  ) : (
-    <div className="rating_widget">
-      <div className="header">
-        <div className="rating_stars">
-          <Rating rating={productRating} />
-          <h3>{`${numberDecimalFix(productRating, 1)} out of 5`}</h3>
-        </div>
         {`${reviewCount} ratings`}
       </div>
       <div className="content">
         {[5, 4, 3, 2, 1].map((starCount) => (
-          <div className="row" key={starCount}>
+          <div
+            className={`row ${ratingMap[starCount] > 0 && 'is_rated'} ${
+              selectedRating() && selectedRating() !== starCount ? 'unfiltered' : ''
+            }`}
+            onClick={() => filterReviews(starCount)}
+            key={starCount}
+          >
             <div className="label">{`${starCount} star`}</div>
             <div className="bar_container">
               {ratingMap[starCount] > 0 && (
