e32a72f11e2575cb2b5e928dee4b80f801c9f4ae
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-12-18 19:10:33 +0200

    Button and Avatar components updated

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -2,39 +2,35 @@ import React from 'react';
 import { randomNumber } from '../constants/utility-functions';
 import './styles/Avatar.css';
 
-const Avatar = ({ type, imageUrl, fullName }) => {
+// classes: image_only, name_only, small
+
+const Avatar = ({ classes, imageUrl, fullName }) => {
   const firstName = fullName?.split(' ')[0];
   const firstInitials = `${fullName?.split(' ')[0][0]}${fullName?.split(' ')[1][0]}`;
 
   const backgroundColors = ['peeps', 'watermelon', 'mimosa', 'kiwi', 'hendrix', 'thanos'];
 
   return (
-    <div className={`avatar card ${type?.includes('small') ? 'small' : ''}`}>
-      {!type?.includes('name_only') && (
-        <div className="image card">
-          {imageUrl ? (
-            <img src={imageUrl} alt="user" />
-          ) : fullName ? (
-            <div
-              className="initials"
-              style={{
-                background: `var(--${backgroundColors[randomNumber(0, backgroundColors.length - 1)]})`
-              }}
-            >
-              {firstInitials}
-            </div>
-          ) : (
-            <i className="fa fa-user"></i>
-          )}
-        </div>
-      )}
-      {fullName && !type?.includes('image_only') && (
-        <div className="name">{type?.includes('header') ? firstName : fullName}</div>
-      )}
+    <div className={`avatar card ${classes ? classes : ''}`}>
+      <div className="image card">
+        {imageUrl ? (
+          <img src={imageUrl} alt={fullName || 'image'} />
+        ) : fullName ? (
+          <div
+            className="initials"
+            style={{
+              background: `var(--${backgroundColors[randomNumber(0, backgroundColors.length - 1)]})`
+            }}
+          >
+            {firstInitials}
+          </div>
+        ) : (
+          <i className="fa fa-user"></i>
+        )}
+      </div>
+      {fullName && !classes?.includes('large') && <div className="name">{classes?.includes('header') ? firstName : fullName}</div>}
     </div>
   );
 };
 
 export default Avatar;
-
-//${currentUser?.userId === authorId ? 'you' : fullName}
--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './styles/Button.css';
 
+// classes:
+// rounding: rounded, circle
+// sizes: small, medium, large
+// colors: white, orange, red, blue
+// types: icon, text
+
 const Button = ({ classes, children, disabled, onClick }) => {
   return (
     <button className={`button_container ${classes}`} onClick={onClick} disabled={disabled}>
