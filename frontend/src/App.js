2b0a68ff9eb2b171561a9f4e2f4fe6061a935aed
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-01-01 19:56:12 +0200

    Deleted Old Unused Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,12 +12,6 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
-// import UserListScreenAdmin from './screens/UserListScreenAdmin';
-// import UserEditScreenAdmin from './screens/UserEditScreenAdmin';
-// import ProductListScreenAdmin from './screens/ProductListScreenAdmin';
-// import ProductEditScreenAdmin from './screens/ProductEditScreenAdmin';
-// import ProductCreateScreenAdmin from './screens/ProductCreateScreenAdmin';
-// import OrderListScreenAdmin from './screens/OrderListScreenAdmin';
 import ProductListScreen from './screens/ProductListScreen';
 import BrowsingHistoryScreen from './screens/BrowsingHistoryScreen';
 import WishListScreen from './screens/WishListScreen';
@@ -49,12 +43,8 @@ const App = () => {
       <Route path="/placeorder" component={PlaceOrderScreen} />
       <Route path="/order/:id" component={OrderScreen} />
       <Route path="/admin/main/:section" component={AdminScreen} />
-      {/* <Route path="/admin/user/:id/edit" component={UserEditScreenAdmin} /> */}
       <Route path="/admin/product/create" component={ProductEditScreen} />
       <Route path="/admin/product/:productId/edit" component={ProductEditScreen} />
-      {/* <Route path="/admin/orderlist" component={OrderListScreenAdmin} /> */}
-      {/* <Route path="/admin/userlist" component={UserListScreenAdmin} /> */}
-      {/* <Route path="/admin/productlist" component={ProductListScreenAdmin} /> */}
       <Route path="/products/:productId" component={ProductScreen} />
       <Route path="/reviews/:productId" component={ReviewsScreen} />
       <Route path="/questions/:productId" component={QuestionsAndAnswersScreen} />
