fefa64d1859492d0318b8b65fa5beca76912d8c9
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-01-05 22:04:35 +0200

    Product tile - master component

--- frontend/src/components/ProductTiles/ProductTile.js
+++ frontend/src/components/ProductTiles/ProductTile.js
@@ -1,18 +1,65 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Button from '../Button';
+import Price from '../Price';
 import './style/ProductTile.css';
 
-const ProductTile = ({ title, items }) => {
-  const contentToShow = items?.map((item) => (
-    <li className="sub_tile" key={item.subTitle}>
-      <img src={item.image} alt={item.subTitle} />
-      <span>{item.subTitle}</span>
-    </li>
-  ));
+const ProductTile = ({
+  products,
+  header,
+  itemSubtitleLine1,
+  itemSubtitleLine2,
+  footer,
+  footerLink
+}) => {
+  const history = useHistory();
 
   return (
     <div className="product_tile card">
-      <div className="title">{title}</div>
-      <div className="content">{contentToShow}</div>
+      <h2 className="header">{header}</h2>
+      <ul className={`content ${products?.length === 4 ? 'four_items' : ''}`}>
+        {products?.map((product) => (
+          <li key={product?.productId} className="card">
+            <Link
+              to={
+                itemSubtitleLine1 === 'brand'
+                  ? `/store/${product?.brand}`
+                  : `/products/${product?.productId}`
+              }
+            >
+              <img src={product?.image} alt={product?.title} />
+              {product?.title && (
+                <div className="product_subtitle">
+                  {itemSubtitleLine1 && (
+                    <div className="subtitle_1">
+                      {product[itemSubtitleLine1] || itemSubtitleLine1}
+                    </div>
+                  )}
+                  {itemSubtitleLine2 && (
+                    <div className="subtitle_2">
+                      {itemSubtitleLine2 === 'price' ? (
+                        <Price
+                          price={product?.price}
+                          size={`${products?.length === 4 ? 'small' : ''}`}
+                        />
+                      ) : (
+                        product[itemSubtitleLine2] || itemSubtitleLine2
+                      )}
+                    </div>
+                  )}
+                </div>
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {footer && (
+        <div className="footer">
+          <Button classes="text" onClick={() => history.push(footerLink)}>
+            {footer}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
