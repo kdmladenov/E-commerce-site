5a20f9ce568a096d0bba5692eb340b1df99a7b26
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-02-01 22:12:17 +0200

    Homescreen - added new ProductTiles

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -63,162 +63,136 @@ const HomeScreen = () => {
         <div className="product_tiles_row_1">
           <div className="tile_1">
             <ProductTile
-              products={['Apple', 'HP', 'Lenovo', 'Dell'].map((brand) =>
-                products.find((product) => product.brand === brand)
-              )}
-              itemSubtitleLine1="brand"
-              header="Shop by brand"
+              header="Best sellers"
+              products={products.sort((a, b) => b.salesCount - a.salesCount).slice(0, 4)}
+              itemSubtitleLine1="title"
+              itemSubtitleLine2="price"
             />
           </div>
           <div className="tile_2">
             {cartItems?.length > 0 ? (
               <ProductTile
+                header="Your cart"
                 products={cartItems.slice(0, cartItems?.length >= 4 ? 4 : 1)}
                 itemSubtitleLine1="title"
                 itemSubtitleLine2="price"
-                header="Your cart"
                 footer="Go to cart"
                 footerLink="/cart"
               />
             ) : (
               <ProductTile
+                header="Screen 14' and down"
                 products={products.filter((product) => product.screenSize >= 14).slice(0, 4)}
                 itemSubtitleLine1="title"
-                header="Screen 14' and down"
               />
             )}
-            {/* <Carousel title="Best value">
-            <ul>
-              {products
-                .sort((a, b) => a.price - b.price)
-                .map((product) => (
-                  <li key={product.productId}>
-                    <Link to={`/products/${product.productId}`}>
-                      <img src={product.image} alt={product.title} />
-                      <div className="content">
-                        <div className="title">{product.title}</div>
-                        <Price price={product?.price} size="small" />
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-          </Carousel> */}
           </div>
           <div className="tile_3">
             {userInfo?.token && products && browsingHistory?.length > 0 ? (
               <ProductTile
+                header="Last visited"
                 products={browsingHistory?.slice(0, browsingHistory?.length >= 4 ? 4 : 1)}
                 itemSubtitleLine1="title"
                 itemSubtitleLine2="price"
-                header="Last visited"
                 footer="View browsing history"
                 footerLink="/history"
               />
             ) : (
               <ProductTile
+                header="Price $900 and up"
                 products={products.filter((product) => product.price >= 900).slice(0, 4)}
                 itemSubtitleLine1="title"
-                header="Price $900 and up"
               />
             )}
           </div>
           <div className="tile_4">
             {userInfo?.token && products && wishList?.length > 0 ? (
               <ProductTile
+                header="Your wish list"
                 products={wishList?.slice(0, wishList?.length >= 4 ? 4 : 1)}
                 itemSubtitleLine1="title"
                 itemSubtitleLine2="price"
-                header="Your wish list"
                 footer="View wish list"
                 footerLink="/wishlist"
               />
             ) : (
               <ProductTile
+                header="Chrome OS laptops"
                 products={products
                   .filter((product) => product.operatingSystem === 'Chrome OS')
                   .slice(0, 4)}
                 itemSubtitleLine1="title"
-                header="Chrome OS laptops"
               />
             )}
           </div>
           <div className="tile_5">
-            {/* <ProductTile
-                products={products.sort((a, b) => a.price - b.price).slice(0, 4)}
-                itemSubtitleLine2="price"
-                header="Most affordable"
-                footer="View all products"
-                footerLink="/productlist"
-              /> */}
             <ProductTile
+              header="Deal of the day"
               products={products.sort((a, b) => b.discount - a.discount).slice(0, 1)}
               itemSubtitleLine1="Today only: up to 30% off"
               itemSubtitleLine2="price"
-              header="Deal of the day"
             />
           </div>
         </div>
         <div className="product_tiles_row_2">
           <div className="tile_6">
             <ProductTile
-              products={products.sort((a, b) => b.rating - a.rating).slice(0, 4)}
               header="Highest rating"
+              products={products.sort((a, b) => b.rating - a.rating).slice(0, 4)}
               footer="View all products"
+              itemSubtitleLine1="title"
+              itemSubtitleLine2="rating"
               footerLink="/productlist"
             />
           </div>
           <div className="tile_7">
             <ProductTile
+              header="Newest products"
               products={products.sort((a, b) => b.productId - a.productId).slice(0, 4)}
               itemSubtitleLine1="title"
-              header="Newest products"
               footer="View all products"
               footerLink="/productlist"
             />
           </div>
           <div className="tile_8">
             <ProductTile
-              products={products.filter((product) => product.processorBrand === 'AMD').slice(0, 4)}
+              header="Most reviewed"
+              products={products.sort((a, b) => b.reviewCount - a.reviewCount).slice(0, 4)}
               itemSubtitleLine1="title"
-              header="AMD laptops"
             />
           </div>
           <div className="tile_9">
             <ProductTile
-              products={products
-                .filter((product) => product.processorBrand === 'Intel')
-                .slice(0, 4)}
+              header="Most wished"
+              products={products.sort((a, b) => b.wishedCount - a.wishedCount).slice(0, 4)}
               itemSubtitleLine1="title"
-              header="Intel laptops"
-              footer="View all Intel laptops"
-              footerLink="/search/Intel"
+              itemSubtitleLine2="price"
             />
           </div>
           <div className="tile_10">
             <ProductTile
-              products={products.filter((product) => product.graphicsType === 'Dedicated')}
+              header="Most popular"
+              products={products.sort((a, b) => b.visitedCount - a.visitedCount).slice(0, 4)}
               itemSubtitleLine1="title"
               itemSubtitleLine2="price"
-              header="Dedicated graphics"
             />
           </div>
         </div>
         <div className="product_tiles_row_3">
           <div className="tile_11">
             <ProductTile
-              products={products
-                .filter((product) => product.operatingSystem === 'Chrome OS')
-                .slice(0, 4)}
-              itemSubtitleLine1="title"
-              header="Chromebooks"
+              header="Shop by brand"
+              products={['Apple', 'HP', 'Lenovo', 'Dell'].map((brand) =>
+                products.find((product) => product.brand === brand)
+              )}
+              itemSubtitleLine1="brand"
             />
           </div>
-          <div className="carousel_os">
-            <Carousel title={'Windows Laptops'}>
+          <div className="carousel_4 card">
+            <Carousel title={'Best value laptops'}>
               <ul>
                 {products
-                  .filter((product) => product.operatingSystem.includes('Windows'))
+                  .sort((a, b) => a.price - b.price)
                   .map((product) => (
                     <li key={product.productId}>
                       <Link to={`/products/${product.productId}`}>
@@ -248,7 +222,7 @@ const HomeScreen = () => {
           </div>
         </div>
 
-        <div className="carousel_1">
+        <div className="carousel_1 card">
           <Carousel title={'Newest Products'}>
             <ul>
               {products
@@ -264,7 +238,7 @@ const HomeScreen = () => {
           </Carousel>
         </div>
 
-        <div className="carousel_2">
+        <div className="carousel_2 card">
           <Carousel title={'All Laptops'}>
             <ul>
               {products
@@ -289,7 +263,7 @@ const HomeScreen = () => {
         </div>
 
         {userInfo?.token && (
-          <div className="carousel_3">
+          <div className="carousel_3 card">
             <Carousel title={'Your Browsing History'}>
               <History horizontal={true} />
             </Carousel>
